3b1a5877df4b8ec649701e38b87d75ae0f0ed7a2
Al-Ghani Desta Setyawan <kou.real@outlook.com>
2025-02-27 15:54:12 +0800

    update package.json and Footer component with new SVG icons and additional metadata

--- src/components/Global_Component/Footer.tsx
+++ src/components/Global_Component/Footer.tsx
@@ -339,14 +339,11 @@ const Footer: React.FC = () => {
                 href="#"
                 className="flex items-center justify-center px-4 py-2 border border-gray-600 rounded-lg hover:bg-gray-700 transition-colors duration-200"
               >
-                <svg
-                  className="h-6 w-6 mr-2 text-blue-400"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M20.55,10.26l-6.25-3.61c-0.58-0.34-1.17-0.37-1.66-0.08L5.38,10.26c-0.44,0.25-0.7,0.67-0.7,1.12c0,0.45,0.26,0.87,0.7,1.12l1.5,0.86v3.85c0,0.33,0.17,0.63,0.44,0.8C7.59,18.17,8.53,18.64,12,18.64s4.41-0.47,4.68-0.63c0.27-0.17,0.44-0.47,0.44-0.8v-3.85l1.5-0.86c0.44-0.25,0.7-0.67,0.7-1.12C21.25,10.93,20.99,10.51,20.55,10.26z" />
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                  height="24" width="24" viewBox="0 0 512 512">
+                  <path fill="#ffffff" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/>
                 </svg>
-                <div>
+                <div className="ml-2">
                   <div className="text-xs text-gray-300">GET IT ON</div>
                   <div className="text-sm font-semibold text-white">
                     Google Play
@@ -357,14 +354,12 @@ const Footer: React.FC = () => {
                 href="#"
                 className="flex items-center justify-center px-4 py-2 border border-gray-600 rounded-lg hover:bg-gray-700 transition-colors duration-200"
               >
-                <svg
-                  className="h-6 w-6 mr-2 text-blue-400"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M17.05,20.25a1,1,0,0,1-.5-.13l-4.55-2.64-4.55,2.64a1,1,0,0,1-1-.08,1,1,0,0,1-.46-.92V4.38A1,1,0,0,1,7,3.38H17.05a1,1,0,0,1,1,1V19.12a1,1,0,0,1-.46.92A1,1,0,0,1,17.05,20.25ZM12,15.88a1,1,0,0,1,.5.13l3.55,2.06V4.38H8V18.07l3.55-2.06A1,1,0,0,1,12,15.88Z" />
-                </svg>
-                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                  height="28" width="21" viewBox="0 0 384 512">
+                  <path fill="#ffffff" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/>
+                  </svg>
+
+                <div className="ml-2"> 
                   <div className="text-xs text-gray-300">Download on the</div>
                   <div className="text-sm font-semibold text-white">
                     App Store
